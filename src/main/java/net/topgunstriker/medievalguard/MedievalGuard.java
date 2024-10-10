51ccfe49f6e0b1586ce1ec2bf98b9fbc878d6e61
topgunstriker <jacksoncampbellmo.360@gmail.com>
2024-10-10 13:53:29 -0500

    medieval_guard_1.21

---
+++ src/main/java/net/topgunstriker/medievalguard/MedievalGuard.java
@@ -0,0 +1,16 @@
+package net.topgunstriker.medievalguard;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MedievalGuard implements ModInitializer {
+	public static final String MOD_ID = "medievalguard";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/topgunstriker/medievalguard/medievalguardclient.java
@@ -0,0 +1,10 @@
+package net.topgunstriker.medievalguard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class medievalguardclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
